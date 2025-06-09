90540a56d4b5b89d6441dbb1a0bc3d484d91b489
Collette <cece554@gmail.com>
2025-06-09 11:55:12 -0500

    add score tracking for letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -38,16 +38,28 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
-  // const point_values = {
-  //       1: "A, E, I, O, U, L, N, R, S, T",
-  //       2: "D, G",
-  //       3: "B, C, M, P",
-  //       4: "F, H, V, W, Y",
-  //       5: "K",
-  //       8: "J, X",
-  //       10: "Q, Z"
-  //   }
+  const pointValues = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+  };
+  
+  let score = 0;
+  const upperWord = word.toUpperCase();
+  
+  for (const letter of upperWord) {
+    score += pointValues[letter] || 0;
+  }
+  
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  }
+  
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
