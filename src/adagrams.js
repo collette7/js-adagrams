c581bf7137daa0e5dd89f593f50d3f7cf7650ae1
Collette <cece554@gmail.com>
2025-06-08 22:23:16 -0500

    Fix tests, and implement draw letters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,25 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterPool = {
+    A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1,
+    K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6,
+    U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+  };
+  
+  const letters = [];
+  for (const [letter, count] of Object.entries(letterPool)) {
+    for (let i = 0; i < count; i++) {
+      letters.push(letter);
+    }
+  }
+  
+  const hand = [];
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    hand.push(letters[randomIndex]);
+    letters.splice(randomIndex, 1);
+  }
+  
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -8,6 +28,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  // const point_values = {
+  //       1: "A, E, I, O, U, L, N, R, S, T",
+  //       2: "D, G",
+  //       3: "B, C, M, P",
+  //       4: "F, H, V, W, Y",
+  //       5: "K",
+  //       8: "J, X",
+  //       10: "Q, Z"
+  //   }
 };
 
 export const highestScoreFrom = (words) => {
